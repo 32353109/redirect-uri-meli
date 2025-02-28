c5d4821086d1d718d13f0fe0e983ca29c4fdad55
32353109 <jota.pedro.coutinho@gmail.com>
2025-02-28 09:51:09 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/oauth/callback')
+def callback():
+    code = request.args.get('code')
+    if code:
+        print(f"Código de autorização recebido: {code}")
+        return "Código recebido com sucesso!"
+    else:
+        return "Erro: Código de autorização não recebido.", 400
+
+@app.route('/')
+def home():
+    return "Bem-vindo ao servidor de redirecionamento!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
