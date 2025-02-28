4130d5f022fc0ef749ce5a1ccae76bf244d3d2d0
32353109 <jota.pedro.coutinho@gmail.com>
2025-02-28 10:22:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, redirect, request
 
 app = Flask(__name__)
@@ -16,4 +17,4 @@ def home():
     return "Bem-vindo ao servidor de redirecionamento!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
