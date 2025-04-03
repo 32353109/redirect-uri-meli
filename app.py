b0bc9cb0af457ffd281f8192bef1bde857fb40f9
32353109 <jota.pedro.coutinho@gmail.com>
2025-04-03 15:24:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def home():
     return "Bem-vindo ao servidor de redirecionamento!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
